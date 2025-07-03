28ad06b73d90894af3c3ae3ef9ab6cbea2b6f029
Mike Wallis <mike@example.com>
2025-07-04 08:42:23 +1200

    Update 7b

--- app.py
+++ app.py
@@ -45,3 +45,7 @@ async def webhook(request: Request):
 
     except Exception as e:
         return {"error": str(e)}
+
+        if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=10000)
