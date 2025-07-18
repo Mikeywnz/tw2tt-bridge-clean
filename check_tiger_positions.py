83153624ffc7c9b09657da0ab15435727a4e1a9a
Mike Wallis <mike@example.com>
2025-07-18 23:42:53 +1200

    EMA Kilo Foxtrot

--- check_tiger_positions.py
+++ check_tiger_positions.py
@@ -2,11 +2,14 @@ from tigeropen.tiger_open_config import TigerOpenClientConfig
 from tigeropen.trade.trade_client import TradeClient
 from tigeropen.common.consts import SegmentType, OrderStatus
 
+config = TigerOpenClientConfig()
+client = TradeClient(config)
+
 orders = client.get_orders(
     account="21807597867063647",
     seg_type=SegmentType.FUT,
     start_time="2025-07-17",
     end_time="2025-07-18",
-    states=["Filled", "Cancelled"],  # 👈 crucial to see historical orders
+    states=["Filled", "Cancelled"],
     limit=100
 )
