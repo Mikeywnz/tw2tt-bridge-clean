caa5e996c5631425043fac3a3e9f2b825d35642a
Mike Wallis <mike@example.com>
2025-07-20 14:05:10 +1200

    EMA Charlie Golf

--- push_orders_loop.py
+++ push_orders_loop.py
@@ -1,12 +1,8 @@
 import time
-from push_orders_to_firebase import push_orders_main  # see below
+from push_orders_to_firebase import push_orders_main  # Make sure this matches your file structure
 
-heartbeat_counter = 0
 while True:
     print("🔄 Running push_orders_main()...")
     push_orders_main()
-    heartbeat_counter += 1
-    if heartbeat_counter >= 6:  # Every 1 minute
-        print("🫀 Order Worker Happy: Still Running...")
-        heartbeat_counter = 0
+    print("🔁 Worker Happy: Still Running...")
     time.sleep(30)
